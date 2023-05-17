86b0d5445eab18941b99ed71cb2ea7e596a0b22e
georgeokez <georgeokez@gmail.com>
2023-05-17 23:54:57 +0100

    feat: added intermediate package

---
+++ intermediate/todo-app-main/script.js
@@ -0,0 +1,7 @@
+// Add onClick event to todo-input-btn
+
+// Add functionality to onClick Event of todo-input-btn
+
+// Create function that creates new todoItem
+
+// Add todo item into todo-items element in DOM
--- multi-step-form/scripts/FlowManager.js
+++
@@ -1,9 +0,0 @@
-export default class FlowManager {
-  constructor(currentStep, steps) {
-    (this.currentStep = currentStep), (this.steps = steps);
-  }
-
-  getCurrentStep() {
-    return this.steps[this.currentStep];
-  }
-}
